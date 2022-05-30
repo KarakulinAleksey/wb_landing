9f49439d3bc78244b05b07fec587578f4ff5de6d
KarakulinAleksey <mail.kotbegemot@ya.ru>
2022-05-30 17:18:28 +0500

    createElement

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,15 @@ import {
   headersContentType
 } from '../utils/var.js';
 import Api from '../components/api.js'
-
-
+const controlPanel = document.querySelector('.content-block__control-panel');
 
 controlPanelButtonLamp.forEach((item)=>{
-  console.log(item);
+  console.log('item', item);
   item.addEventListener('click', function(evt){
     console.log(evt.target);
+
   })
 })
-// console.log(controlPanelButtonLamp);
 
 const api = new Api({
   baseUrl: baseUrl,
@@ -23,10 +22,31 @@ const api = new Api({
   },
 });
 
-const allLamp = api.getAllLamp();
-console.log(allLamp);
+const getAllLamp = api.getAllLamp();
 
-allLamp
+getAllLamp
   .then((data)=>{
-    console.log(data);
+    console.log('data', data);
+    addElement(data);
   })
+  .catch((err) => {
+    console.log('Error ' + err);
+  })
+
+  addElement();
+
+function addElement(data){
+  const buttomElement = [];
+  for (let i=0;i<3;i++){
+    const listItem = document.createElement('li');
+    listItem.classList.add('content-block__control-panel_item');
+    const imageListItem = document.createElement('img');
+    imageListItem.src = data.i.image;
+    imageListItem.classList.add('content-block__control-panel_image-light');
+    listItem.prepend(imageListItem);
+    buttomElement[i]=listItem;
+  }
+  for (let i=0;i<3;i++){
+    controlPanel.prepend(buttomElement[i]);
+  }
+}
