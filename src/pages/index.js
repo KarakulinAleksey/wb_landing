d7d7b2457105868212b0b72d525285c90baba499
KarakulinAleksey <mail.kotbegemot@ya.ru>
2022-05-31 16:58:11 +0500

    add information

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import './index.css';
 import {
   controlPanelButtonLamp,
+  viewBlockLamp,
+  viewBlock,
+  bottonLight,
+  bottonNight,
+  contentBlockTextMaterial,
   baseUrl,
   headersContentType
 } from '../utils/var.js';
-import Api from '../components/api.js'
-const controlPanel = document.querySelector('.content-block__control-panel');
+import Api from '../components/api.js';
 
-controlPanelButtonLamp.forEach((item)=>{
-  console.log('item', item);
-  item.addEventListener('click', function(evt){
-    console.log(evt.target);
+bottonNight.addEventListener('click',()=>{
+  viewBlock.classList.add('view-block__background-night');
+})
 
-  })
+bottonLight.addEventListener('click',()=>{
+  viewBlock.classList.remove('view-block__background-night')
 })
 
 const api = new Api({
@@ -22,37 +26,50 @@ const api = new Api({
   },
 });
 
-const getAllLamp = api.getAllLamp();
+const allLamp = api.getAllLamp();
 
-getAllLamp
+console.log(contentBlockTextMaterial);
+
+allLamp
   .then((data)=>{
-    console.log('data', data);
-    for (let i=0;i<data.length-1;i++){
-      addElement(data[i]);
+    for (let i=0;i<3;i++){
+      addElement(data[i], controlPanelButtonLamp[i]);
     }
-    // data.forEach((item)=>{
-    //   addElement(item);
-    // })
+    console.log(data);
+    viewBlockLamp.src = data[0].image;
+    controlPanelButtonLamp[0].classList.add('content-block__control-panel_item-focus');
   })
   .catch((err) => {
     console.log('Error ' + err);
   })
 
-
-
-function addElement(data){
-  // const buttomElement = [];
-  // for (let i=0;i<3;i++){
-    const listItem = document.createElement('li');
-    listItem.classList.add('content-block__control-panel_item');
+function addElement(data, panelItem){
     const imageListItem = document.createElement('img');
     imageListItem.src = data.image;
     imageListItem.classList.add('content-block__control-panel_image-light');
-    listItem.prepend(imageListItem);
-    // buttomElement[i]=listItem;
-  // }
-  // for (let i=0;i<1;i++){
-    // controlPanel.prepend(buttomElement[i]);
-    controlPanel.prepend(listItem);
-  // }
+    panelItem.addEventListener('click', function(evt){
+      viewBlockLamp.src = data.image;
+      resetBackgroundControlPanelButtonLamp ();
+      panelItem.classList.add('content-block__control-panel_item-focus');
+      contentBlockTextMaterial[0].innerHTML=upperCase(data.material);
+      contentBlockTextMaterial[1].innerHTML='H' + data.height + ' x ' + 'W' + data.width + ' x ' + 'D15';
+      contentBlockTextMaterial[2].innerHTML=data.weight + ' kg';
+      contentBlockTextMaterial[3].innerHTML=joinText(data.electrification);
+    })
+    panelItem.prepend(imageListItem);
 }
+
+function resetBackgroundControlPanelButtonLamp (){
+  controlPanelButtonLamp.forEach((item)=>{
+    item.classList.remove('content-block__control-panel_item-focus');
+  })
+}
+
+function upperCase(text){
+  return ((text.slice(0,1).toUpperCase()) + text.slice(1));
+}
+
+function joinText(text){
+  return((text.split(',').join(' |')));
+}
+
--- src/utils/var.js
+++ src/utils/var.js
@@ -1,4 +1,10 @@
 export const controlPanelButtonLamp = document.querySelectorAll('.content-block__control-panel_item');
+export const viewBlockLamp = document.querySelector('.view-block__lamp');
+export const viewBlock = document.querySelector('.view-block');
+export const bottonLight = document.querySelector('.content-block__control-panel_botton-light');
+export const bottonNight = document.querySelector('.content-block__control-panel_botton-night');
+export const contentBlockTextMaterial = document.querySelectorAll('.content-block__text.text-light');
+
 
 export const baseUrl = 'https://private-anon-f453b48ec0-lampshop.apiary-mock.com/lamps';
 export const headersContentType = 'application/json';
